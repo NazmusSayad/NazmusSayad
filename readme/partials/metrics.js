a4eeabf7422e3c2341a1c8b632298ddabef0b37c
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-10-30 03:31:51 +0600

    Update metrics.js

--- readme/partials/metrics.js
+++ readme/partials/metrics.js
@@ -1,7 +1,7 @@
 exports.path = __dirname + '/metrics.md'
 exports.placeHolders = {
   githubMetrics:
-    'https://github.com/NazmusSayad/github.statistics--/raw/master/github-metrics.svg',
+    'https://github.com/NazmusSayad/Git-Stats/raw/master/github-metrics.svg',
   activityGraph:
     'https://activity-graph.herokuapp.com/graph?username=NazmusSayad&bg_color=161B22&color=9e9e9e&line=bababa&point=a76c6c&area=true&hide_border=true&hide_title=true',
 }
